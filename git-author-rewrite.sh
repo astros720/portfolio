14cd46da062fd75a04143606e79a2a9e72efe95f
jesse <jessewarfel96@gmail.com>
2024-08-24 13:29:36 -0400

    initial portfolio

---
+++ git-author-rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="astrostar47@outlook.com"
+CORRECT_NAME="jesse"
+CORRECT_EMAIL="jessewarfel96@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
